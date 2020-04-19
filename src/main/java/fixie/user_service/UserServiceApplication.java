9c356f76d214bb43c4f36f02544756a182c45ba8
Artur Wita <witart1998@gmail.com>
2020-04-19 16:00:51 +0200

    default tomcat port changed

--- src/main/java/fixie/user_service/UserServiceApplication.java
+++ src/main/java/fixie/user_service/UserServiceApplication.java
@@ -1,24 +1,18 @@
 package fixie.user_service;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
-	// To delete in future
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		SpringApplication app = new SpringApplication(UserServiceApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "8000"));
+		app.run(args);
 	}
 
 }
