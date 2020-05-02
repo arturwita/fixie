a6c8077cc9c7cb3e60673e075a196f9e4a983052
PawelZaton <32542079+PawelZaton@users.noreply.github.com>
2020-05-02 12:00:38 +0200

    Delete UserServiceUtils.java

--- src/main/java/fixie/user_service/utils/UserServiceUtils.java
+++
@@ -1,9 +0,0 @@
-package fixie.user_service.utils;
-
-import org.springframework.security.crypto.bcrypt.BCrypt;
-
-public class UserServiceUtils {
-    public static String hashPassword(String password, int saltRounds) {
-        return BCrypt.hashpw(password, BCrypt.gensalt(saltRounds));
-    }
-}
